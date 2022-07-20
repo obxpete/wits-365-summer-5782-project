48c0bdd943f7f12c54914d4004b5ffbe2cefc93a
Peter Lombardo <obxpete@gmail.com>
2022-07-20 18:14:36 -0400

    adds individula tasks to memory (validated)

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,20 +1,37 @@
-import { Component } from '@angular/core';
+import { Component , OnInit} from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import {  FormGroup,  Validators,  FormBuilder} from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
   title = 'frontend';
-  taskData:[];
-  constructor(private http: HttpClient) {
+  taskForm : FormGroup;
+  taskData:object[];
+    
+    constructor(private http: HttpClient,private formBuilder: FormBuilder) {
+    
+      //basic angular form
+      this.taskForm = this.formBuilder.group({
+        newTask: this.formBuilder.control(''),
+      })
 
-  }
-  getTasks() {
+    }
+
+  // ngOnInit runs when the component loads
+  ngOnInit(): void {
     this.http.get<[]>('/api').subscribe(data => {
       this.taskData = data;
-  })  
+    })  
   }
+
+
+  addTask() {
+    this.taskData.push({task:this.taskForm.get('newTask').value});
+    this.taskForm.get('newTask').reset()
+  }
+
 }
