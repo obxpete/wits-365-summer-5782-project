7006cfa44c8346f2a0add3e2063aabd0ba1b3015
Peter Lombardo <obxpete@gmail.com>
2022-07-27 18:25:52 -0400

    adds matt table for query results

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,6 +1,7 @@
 import { Component , OnInit} from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import {  FormGroup,  Validators,  FormBuilder} from '@angular/forms';
+import { MatTable, MatTableDataSource} from '@angular/material/table';
 
 @Component({
   selector: 'app-root',
@@ -9,22 +10,36 @@ import {  FormGroup,  Validators,  FormBuilder} from '@angular/forms';
 })
 export class AppComponent implements OnInit{
   title = 'frontend';
+  displayedColumns: string[] = ['taskID', 'task'];
+
+  ELEMENT_DATA: task[] = [{
+    'taskID': 0,
+    'task': ''
+  }]
+
+
+  dataSource = new MatTableDataSource<task>(this.ELEMENT_DATA);
+ 
+
   taskForm : FormGroup;
   taskData:object[];
     
-    constructor(private http: HttpClient,private formBuilder: FormBuilder) {
-    
-      //basic angular form
-      this.taskForm = this.formBuilder.group({
-        newTask: this.formBuilder.control(''),
-      })
-
-    }
+  constructor(private http: HttpClient,private formBuilder: FormBuilder) {
+    //basic angular form
+    this.taskForm = this.formBuilder.group({
+      newTask: this.formBuilder.control(''),
+    })
+  }
 
   // ngOnInit runs when the component loads
   ngOnInit(): void {
-    this.http.get<[]>('/api').subscribe(data => {
+  this.ELEMENT_DATA = [{taskID: 1, task: 'Hydrogen'}]
+
+    this.http.get<[]>('http://localhost:8090/api/tasks').subscribe(data => {
       this.taskData = data;
+      this.ELEMENT_DATA = data['recordset'].map(task => {return {taskID: task.taskID, task: task.task}})
+      this.dataSource = new MatTableDataSource<task>(this.ELEMENT_DATA);
+      console.log(this.ELEMENT_DATA)
     })  
   }
 
@@ -34,4 +49,10 @@ export class AppComponent implements OnInit{
     this.taskForm.get('newTask').reset()
   }
 
+}
+
+
+export interface task {
+  taskID: number;
+  task: string;
 }
