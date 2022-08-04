de20cc53f0881b2cb170d7016828d094033d0e19
WITS Student <witsstudent@WITSs-Mac-mini.local>
2022-08-03 21:40:38 -0400

    Add update, delete and add functionality

--- backend/api.js
+++ backend/api.js
@@ -46,11 +46,20 @@ router.route('/add').post((request, response) =>{
 })
 
 
+router.route('/update/').post((request, response) =>{
+    let task = {... request.body};
+    dbOperations.updateTask({taskID: task.taskID,  task:task.task }).then(result => {
+        response.status(201).json(result)
+    }) 
+})
 
-// Insert Update Route Here
 
-
-// Insert Delete Route Here
+router.route('/delete/:taskID').get((request, response) =>{
+    let result
+    dbOperations.deleteTask(request.params.taskID).then(result => {
+        response.status(201).json(result)
+    }) 
+})
 
 var port = process.env.PORT ||  8090;
 app.listen(port);
--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -1,16 +1,16 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppComponent } from './app.component';
 import { HttpClientModule } from '@angular/common/http';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MatTableModule } from '@angular/material/table'  
 import { MatToolbarModule } from '@angular/material/toolbar'  
-// update imports here
 import { MatFormFieldModule} from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
 import { MatPaginatorModule } from '@angular/material/paginator'
+import { MatSelectModule } from '@angular/material/select'
+import { MatButtonModule } from '@angular/material/button'
 
 
 @NgModule({
@@ -20,10 +20,12 @@ import { MatPaginatorModule } from '@angular/material/paginator'
   imports: [
     BrowserModule,
     HttpClientModule,
-    
-    // and here (imports array)
+    MatFormFieldModule, MatInputModule,
     MatToolbarModule,
     MatPaginatorModule, MatTableModule,
+    MatSelectModule,
+    MatFormFieldModule, 
+    MatInputModule, MatButtonModule,
     FormsModule, ReactiveFormsModule, BrowserAnimationsModule
 ],
   providers: [],
